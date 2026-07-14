d742ef1ee0b202d1d3d06f881a18924a18b16a00
Prashant Kumar <kumarprashant1345@gmail.com>
2026-07-14 20:53:47 +0530

    weather app completed

--- weather app/src/components/SearchBar.jsx
+++ weather app/src/components/SearchBar.jsx
@@ -13,51 +13,65 @@ function SearchBar() {
   const [cityData, setCityData] = useState(null)
   const [forecast, setForecast] = useState([])
 
+  const getWindDirection = (deg) => {
+
+    if (deg >= 337.5 || deg < 22.5) return "North";
+    if (deg >= 22.5 && deg < 67.5) return "North East";
+    if (deg >= 67.5 && deg < 112.5) return "East";
+    if (deg >= 112.5 && deg < 157.5) return "South East";
+    if (deg >= 157.5 && deg < 202.5) return "South";
+    if (deg >= 202.5 && deg < 247.5) return "South West";
+    if (deg >= 247.5 && deg < 292.5) return "West";
+    if (deg >= 292.5 && deg < 337.5) return "North West";
+
+  }
+
   const getData = async (city) => {
-    const weatherRes = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_WEATHER_API}&units=metric`)
-    const weatherData = await weatherRes.json()
+    try {
+      const weatherRes = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_WEATHER_API}&units=metric`)
+      const weatherData = await weatherRes.json()
 
-    const forecastRes = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${import.meta.env.VITE_WEATHER_API}&units=metric`)
-    const forecastData = await forecastRes.json()
-    setForecast(forecastData.list.filter((item) =>
-      item.dt_txt.includes("12:00:00")))
+      if (weatherData.cod !== 200) {
+        alert("City not found")
+        return;
+      }
+
+      const forecastRes = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${import.meta.env.VITE_WEATHER_API}&units=metric`)
+      const forecastData = await forecastRes.json()
+
+      setForecast(forecastData.list.filter((item) =>
+        item.dt_txt.includes("12:00:00")))
 
 
-    console.log(weatherData);
-    const getWindDirection = (deg) => {
+      console.log(weatherData);
 
-      if (deg >= 337.5 || deg < 22.5) return "North";
-      if (deg >= 22.5 && deg < 67.5) return "North East";
-      if (deg >= 67.5 && deg < 112.5) return "East";
-      if (deg >= 112.5 && deg < 157.5) return "South East";
-      if (deg >= 157.5 && deg < 202.5) return "South";
-      if (deg >= 202.5 && deg < 247.5) return "South West";
-      if (deg >= 247.5 && deg < 292.5) return "West";
-      if (deg >= 292.5 && deg < 337.5) return "North West";
 
+      const sunrise = new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+      const sunset = new Date(weatherData.sys.sunset * 1000).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+      const windSpeed = Math.floor(weatherData.wind.speed * 3.6);
+
+      setCityData({
+        name: weatherData.name,
+        temp: weatherData.main.temp,
+        feelsLike: weatherData.main.feels_like,
+        pressure: weatherData.main.pressure,
+        maxTemp: weatherData.main.temp_max,
+        minTemp: weatherData.main.temp_min,
+        humidity: weatherData.main.humidity,
+        windSpeed: windSpeed,
+        windDirection: getWindDirection(weatherData.wind.deg),
+        windDeg: weatherData.wind.deg,
+        sunrise: sunrise,
+        sunset: sunset,
+        weather: weatherData.weather[0].description,
+        img: weatherData.weather[0].icon,
+        condition: weatherData.weather[0].main
+      })
     }
-
-    const sunrise = new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
-    const sunset = new Date(weatherData.sys.sunset * 1000).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
-    const windSpeed = Math.floor(weatherData.wind.speed * 3.6);
-
-    setCityData({
-      name: weatherData.name,
-      temp: weatherData.main.temp,
-      feelsLike: weatherData.main.feels_like,
-      pressure: weatherData.main.pressure,
-      maxTemp: weatherData.main.temp_max,
-      minTemp: weatherData.main.temp_min,
-      humidity: weatherData.main.humidity,
-      windSpeed: windSpeed,
-      windDirection: getWindDirection(weatherData.wind.deg),
-      windDeg: weatherData.wind.deg,
-      sunrise: sunrise,
-      sunset: sunset,
-      weather: weatherData.weather[0].description,
-      img: weatherData.weather[0].icon,
-      condition: weatherData.weather[0].main
-    })
+    catch (error) {
+      console.log(error)
+      alert("Something went wrong")
+    }
 
   }
   console.log(forecast);
@@ -87,10 +101,13 @@ function SearchBar() {
 
 
 
+
   return (
     <>
+
+
       {cityData && (
-        <div className="mx-auto max-w-3xl min-h-screen bg-cover bg-center pt-4" style={{
+        <div className="mx-auto w-full max-w-3xl min-h-screen bg-cover bg-center pt-4" style={{
           backgroundImage: `url(${weatherImages[cityData.condition]})`
         }}>
           <div className="text-center flex justify-center gap-2 pt-2">
@@ -103,15 +120,15 @@ function SearchBar() {
             <p>{cityData.weather}</p>
           </div>
           <h2 className="text-center text-5xl font-bold">{Math.floor(cityData.temp)}&deg;C</h2>
-          <div className="grid grid-cols-2 mt-4 gap-2 p-2">
-            <div className="p-2 bg-gray-200/25 text-white font-bold backdrop-blur-md rounded-3xl">
+          <div className="grid grid-cols-1  mx-auto md:grid-cols-2 md:w-full mt-4 gap-2 p-2">
+            <div className="p-2 bg-gray-200/25 shadow-lg text-white font-bold backdrop-blur-md rounded-3xl">
               <p>5 day forecast</p>
               <div>
 
                 {forecast.map((day) => (
                   <div className="flex justify-between" key={day.dt_txt}>
                     <div className="flex gap-4">
-                      <img src={`https://openweathermap.org/payload/api/media/file/${cityData.img}.png`} alt="" className="w-8"/>
+                      <img src={`https://openweathermap.org/payload/api/media/file/${day.weather[0].icon}.png`} alt="" className="w-8" />
                       <p>
                         {
                           new Date(day.dt_txt).toDateString() === new Date().toDateString()
@@ -135,9 +152,9 @@ function SearchBar() {
 
 
             </div>
-            <div className="grid grid-cols-2 gap-2 font-bold ">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-2 font-bold ">
               <div className="grid grid-rows-2 gap-2">
-                <div className="flex justify-between bg-gray-200/25 backdrop-blur text-white p-2 rounded-3xl">
+                <div className="flex justify-between bg-gray-200/25 shadow-lg backdrop-blur text-white p-2 rounded-3xl">
                   <div>
                     <p>{cityData.windDirection}</p>
                     <p>{cityData.windSpeed} km/h</p>
@@ -170,12 +187,12 @@ function SearchBar() {
 
 
 
-                <div className="bg-gray-200/25 backdrop-blur text-white p-2 rounded-3xl">
+                <div className="bg-gray-200/25 shadow-lg backdrop-blur text-white p-2 rounded-3xl">
                   <p>Sunrise {cityData.sunrise}</p>
                   <p>Sunset {cityData.sunset}</p>
                 </div>
               </div>
-              <div className="bg-gray-200/25 backdrop-blur text-white p-2 rounded-3xl">
+              <div className="bg-gray-200/25 shadow-lg backdrop-blur text-white p-2 rounded-3xl">
                 <div className="flex justify-between mb-2">
                   <p>Humidity</p>
                   <p>{cityData.humidity}%</p>
