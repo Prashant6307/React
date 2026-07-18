8f37acb516eb51eb67684321295ae5875ae21df4
Prashant Kumar <kumarprashant1345@gmail.com>
2026-07-18 18:08:00 +0530

    added trailer in movie-app

--- movie-app/src/components/MovieDetails.jsx
+++ movie-app/src/components/MovieDetails.jsx
@@ -1,16 +1,27 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { API_KEY } from "../api/tmdb"
 import { useEffect, useRef, useState } from "react"
 import "../App.css"
+import play_btn from "../images/play-btn.png"
 
 function MovieDetails() {
     const [details, setDetails] = useState(null)
     const [cast, setCast] = useState([])
+    const [trailer, setTrailer] = useState(null)
+    const [similarMovies, setSimilarMovies] = useState([])
+    const [showTrailer, setShowTrailer] = useState(false)
+
     const { movieId } = useParams()
+    const navigate = useNavigate()
     const carouselRef = useRef()
-    console.log(cast);
+    const similarRef = useRef()
+
+    console.log(trailer);
 
 
+    // const playTrailer = () => {
+    //     window.open(`https://youtube.com/watch?v=${trailer.key}`)
+    // }
 
 
     useEffect(() => {
@@ -29,32 +40,80 @@ function MovieDetails() {
         }
         getCast(movieId)
 
+        const getTrailer = async (movieId) => {
+            const videoRes = await fetch(
+                `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${API_KEY}`
+            )
+
+            const videoData = await videoRes.json()
+
+            const movieTrailer = videoData.results.find(
+                video => video.type === "Trailer" && video.site === "YouTube"
+            )
+
+            setTrailer(movieTrailer)
+        }
+        getTrailer(movieId)
+
+        const getSimilarMovies = async () => {
+            const res = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/similar?api_key=${API_KEY}`)
+            const data = await res.json()
+            setSimilarMovies(data)
+        }
+        getSimilarMovies(movieId)
+
     }, [movieId])
 
     return (
-        <div className="bg-black">
+        <div className="bg-black ">
             {details && (
                 <div className="max-w-7xl mx-auto">
-                    <div className="relative flex">
+                    <div className="relative flex aspect-video">
 
-                        <img src={`https://image.tmdb.org/t/p/w1280${details.backdrop_path}`} alt={details.title} className="" />
-                        <div className="absolute p-4 text-gray font-bold bg-gradient-to-r from-black to-transparent text-gray-200 h-full ">
-                            <p className="mt-[16%]">{details.title}</p>
-                            <div className="flex items-center gap-8 ">
-                                <p>{(details.vote_average).toFixed(1)}</p>
-                                <img src={`https://image.tmdb.org/t/p/w200${details.production_companies[0]?.logo_path}`} alt="" className="max-w-12" />
-                                <p>{details.runtime && (details.runtime / 60).toFixed(1)}hrs</p>
-                            </div>
-                            <p className="max-w-[50%] text-[8px] sm:text-sm md:text-lg lg:text-xl ">{details.overview}</p>
+                        {
+                            showTrailer && trailer ? (<>
+                                <button
+                                    onClick={() => setShowTrailer(false)}
+                                    className="absolute top-4 right-4 z-10 text-white text-3xl"
+                                >
+                                    ✕
+                                </button>
+                                <iframe
+                                    src={`https://www.youtube.com/embed/${trailer.key}?autoplay=1`}
+                                    title="Trailer"
+                                    className="w-full h-full"
+                                    allow="autoplay; fullscreen"
+                                />
+                            </>
+                            ) : (
+                                <>
+                                    <img
+                                        src={`https://image.tmdb.org/t/p/w1280${details.backdrop_path}`}
+                                        alt={details.title}
+                                        className="w-full h-full object-cover"
+                                    />
+                                    <div className="absolute p-4 text-gray font-bold bg-gradient-to-r from-black to-transparent text-gray-200 h-full ">
+                                        <p className=" text-white text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold tracking-wide drop-shadow-2xl">{details.title}</p>
+                                        <div className="flex items-center gap-8 my-12 text-3xl">
+                                            <p>⭐{(details.vote_average).toFixed(1)}</p>
+                                            <img src={`https://image.tmdb.org/t/p/w200${details.production_companies[0]?.logo_path}`} alt="" className="max-w-12 bg-white m-1" />
+                                            <p>{details.runtime && (details.runtime / 60).toFixed(1)}hrs</p>
+                                        </div>
+                                        <p className="max-w-[50%] text-[8px] sm:text-sm md:text-lg lg:text-xl ">{details.overview}</p>
 
 
-                            <button>Watch Trailer</button>
-                        </div>
+                                        <button onClick={() => setShowTrailer(true)} className="flex items-center mt-4 text-lg">
+                                            <img src={play_btn} alt="" className="w-20 " />
+                                            Watch Trailer</button>
+                                    </div>
+                                </>
+                            )
+                        }
+
                     </div>
 
-                    <h2 className="text-white font-bold text-xl mt-8 mb-4">Cast & Characters</h2>
-
-                    <div className="relative">
+                    <div className="relative p-4">
+                        <h2 className="text-white font-bold text-xl mt-8 mb-4">Cast & Characters</h2>
                         <div ref={carouselRef} className=" flex gap-4 overflow-x-auto scrollbar-hide scroll-smooth ">
 
                             {
@@ -79,22 +138,76 @@ function MovieDetails() {
                                     </div>
                                 ))
                             }
-                            
+
                         </div>
                         <button
-                                onClick={() => {
-                                    carouselRef.current.scrollLeft -= 300
-                                }}
-                                className="absolute left-0 top-1/2 -translate-y-1/2 text-3xl text-white hover:bg-black/65 bg-black/45 rounded-full h-12 w-12 ml-4 text-center pb-1">
-                                ←
-                            </button>
-                            <button
-                                onClick={() => {
-                                    carouselRef.current.scrollLeft += 300
-                                }}
-                                className="absolute right-0 top-1/2 -translate-y-1/2 text-3xl text-white hover:bg-black/65 bg-black/45 rounded-full h-12 w-12 mr-4 text-center pb-1">
-                                →
-                            </button>
+                            onClick={() => {
+                                carouselRef.current.scrollLeft -= 300
+                            }}
+                            className="absolute left-0 top-1/2 -translate-y-1/2 text-3xl text-white hover:bg-black/65 bg-black/45 rounded-full h-12 w-12 ml-4 text-center pb-1">
+                            ←
+                        </button>
+                        <button
+                            onClick={() => {
+                                carouselRef.current.scrollLeft += 300
+                            }}
+                            className="absolute right-0 top-1/2 -translate-y-1/2 text-3xl text-white hover:bg-black/65 bg-black/45 rounded-full h-12 w-12 mr-4 text-center pb-1">
+                            →
+                        </button>
+                    </div>
+
+
+
+                    <div className="relative p-4" >
+                        <h2 className="text-white font-bold text-xl mt-8 ">Similar Movies</h2>
+                        <div ref={similarRef} className="mt-8 flex gap-4 overflow-x-auto scrollbar-hide scroll-smooth " >
+
+                            {
+                                similarMovies.results?.filter((result) => result.backdrop_path).map((result) => (
+                                    <div onClick={() => navigate(`/movie/${result.id}`)}
+                                        key={result.id}
+                                        className="min-w-[150px] bg-gray-800 rounded-xl overflow-hidden"
+                                    >
+
+                                        <img
+                                            src={`https://image.tmdb.org/t/p/w185${result.poster_path}`}
+                                            alt={result.original_title}
+                                            className="w-full h-48 object-cover"
+                                        />
+
+                                        <p className="p-2 font-bold text-gray-300">
+                                            {result.title}
+                                        </p>
+
+
+                                        <div className="px-1 py-2">
+                                            <p className=" px-2 text-sm text-gray-400">
+                                                Rating: ⭐{(result.vote_average).toFixed(1)}
+                                            </p>
+                                            <p className="px-2 text-sm text-gray-400">
+                                                Release: {(result.release_date)}
+                                            </p>
+                                        </div>
+
+                                    </div>
+                                ))
+                            }
+
+                        </div>
+                        <button
+                            onClick={() => {
+                                similarRef.current.scrollLeft -= 300
+                            }}
+                            className="absolute left-0 top-1/2 -translate-y-1/2 text-3xl text-white hover:bg-black/65 bg-black/45 rounded-full h-12 w-12 ml-4 text-center pb-1">
+                            ←
+                        </button>
+                        <button
+                            onClick={() => {
+                                similarRef.current.scrollLeft += 300
+                            }}
+                            className="absolute right-0 top-1/2 -translate-y-1/2 text-3xl text-white hover:bg-black/65 bg-black/45 rounded-full h-12 w-12 mr-4 text-center pb-1">
+                            →
+                        </button>
                     </div>
                 </div>
             )
